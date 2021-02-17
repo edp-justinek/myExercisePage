77abc49df33d528462abae6836c91202cbdc65aa
Justine <jkaeslin@edp.ch>
2021-02-17 22:42:34 +0100

    upload uebungsContent and the way to show the uebung1 in the main page

--- content/Uebung1/scrip.js
+++ content/Uebung1/scrip.js
@@ -1,18 +1,22 @@
+//Sobald das Dokument geladen ist, werden nachfolgende Funktionen ausgelöst
 document.addEventListener("DOMContentLoaded", (event) => {
-
+    //Buttons erhalten eine Funktion, die jeweils per klick ausgelöst wird
     addClickFunction('confirm',countChars);
     addClickFunction('wordAscending',sortLettersAscending);
     // addClickFunction('wordDescending',sortLettersDescending);
     // addClickFunction('NumberAscending',sortNumbersAscending);
     // addClickFunction('NumberDescending',sortNumbersDescending);
 });
-
+//Diese Funktion liest den Wert des Input Felds und wandelt alle Grossbuchstaben in kleine Buchstaben aus
+//danach wird das Wort pro Buchstabe gesplitet zuletzt
+// //wird dieser Liste an die beiden Funktionen "arrayMagic" und "mapMagic" weitergegeben
 function countChars() {
     const inputString = document.querySelector('#textInput').value;
     const charList = inputString.toLowerCase().split('');
-
     arrayMagic(charList);
     mapMagic(charList);
+    sortLettersAscending(charList);
+
 }
 
 function addClickFunction(elementId, func) {
@@ -24,6 +28,7 @@ function addClickFunction(elementId, func) {
 let loopCounterMap1 = 0;
 function mapMagic(charList){
     //Hier wird die funktion mapMagic initialisiert und der input Parameter charList mitgegeben
+    //Objekt wird erstellt
     let resultMap = {};
 
     for(let char of charList){
@@ -48,20 +53,23 @@ let countLoop = 0;
 //Buchstaben zählen 2 For Schleifen
 function arrayMagic(charList){
     //Hier wird die funktion arrayMagic  initialisiert und der input Parameter charList mitgegeben
+    //Speichert jeden Wert nur einmal (jeder Wert ist eindeutig)
     const charSet = new Set(charList);
-    //Speichert jeden Wert nur einmal
+    //hier wird das erste element der Liste genommen und dann über die zweite For-Schleife mit allen Buchstaben
+    //der Liste verglichen und gezählt sobald der Buchstabe gleich ist
     for(let char of charSet){
         let charCounter = 0;
-
         for(let checkChar of charList){
             if(char == checkChar){
                 charCounter++;
             }
             countLoop++;
         }
+        //Danach wird eine liste der Buchstaben und die zählungen der Buchstaben an die id "output" mitgegeben
         displayOutput("output", char, charCounter);
+        //Schleifenzähler wird mit ID an Funktion "displayOutputCounter" weitergegeben
          displayOutputCounter("outputCounter", countLoop);
-        sortLettersAscending( charCounter);
+        sortLettersAscending( charSet);
     }
 
 }
@@ -77,24 +85,15 @@ function displayOutput(domId, char, charCounter){
     document.getElementById(domId).innerHTML += `Output: ${char} Amount: ${charCounter} <br>`;
 }
 
-
-
 //Sort
-function sortLettersAscending(charCounter){
-    let bubbleSort = (charCounter) => {
-        let len = charCounter.length;
-        for (let i = 0; i < len; i++) {
-            for (let j = 0; j < len; j++) {
-                if (charCounter[j] > charCounter[j + 1]) {
-                    let tmp = charCounter[j];
-                    charCounter[j] = charCounter[j + 1];
-                    charCounter[j + 1] = tmp;
-                }
-            }
-        }
-        return charCounter;
-    };
-    document.getElementById("demo").innerHTML += `${charCounter}`;
+function sortLettersAscending(charList){
+    let charCounterArray = charList;
+    console.log(charCounterArray);
+    charCounterArray.sort(function(a, b) {
+         return a - b;
+     });
+
+    document.getElementById('demo').innerHTML = charCounterArray;
 }
 //
 // function sortLettersDescending(demo, char){
--- js/pageSwitcher.js
+++ js/pageSwitcher.js
@@ -1,4 +1,45 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("Hello World");
+    const siteUebung1 = document.getElementById("uebung1").addEventListener("click",changInnerObj1);
+    const siteUebung2 = document.getElementById("uebung2").addEventListener("click",changInnerObj2);
+    const siteUebung3 = document.getElementById("uebung3").addEventListener("click",changInnerObj3);
+    const siteUebung4 = document.getElementById("uebung4").addEventListener("click",changInnerObj4);
+
+    const innerObj = document.getElementById("innerObj")
+
+     const objUebung1 = document.getElementById("obj1");
+     const objUebung2 = document.getElementById("obj2");
+     const objUebung3 = document.getElementById("obj3");
+     const objUebung4 = document.getElementById("obj4");
+
+    // const test1 ='<object data="./content/Uebung1/index.html"></object></object>';
+    // const test2 ='<object data="./content/Uebung2/index.html"></object></object>';
+    // const test3 ='<object data="./content/Uebung3/index.html"></object></object>';
+    // const test4 ='<object data="./content/Uebung4/index.html"></object></object>';
+
+
+ function changInnerObj1(){
+     innerObj.style.display = "none";
+     objUebung1.style.display = "block";
+ }
+
+ function changInnerObj2(){
+     innerObj.style.display = "none";
+     objUebung2.style.display = "block";
+ }
+
+ function changInnerObj3(){
+     innerObj.style.display = "none";
+     objUebung3.style.display = "block";
+ }
+
+ function changInnerObj4(){
+     innerObj.style.display = "none";
+     objUebung4.style.display = "block";
+ }
+
+
+
+
+
 });
