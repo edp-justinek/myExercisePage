cd0b0a6906ba1da1e3bddcbf407c024bbbe88e39
Justine <justine_kaeslin@sluz.ch>
2022-01-04 19:21:28 +0100

    html: -entfernen unnötiger abhängigkeiten -seiten titel definiert -hamburgermenu entfernt -seitenstruktur angepasst --content entschlackt --navigation auf die seite gelegt -aufgeräumt js: -Logik für Inhalts wechsel erstellt

--- js/pageSwitcher.js
+++ js/pageSwitcher.js
@@ -1,71 +1,35 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("navCountChars").addEventListener("click",changInnerObj1);
-    document.getElementById("navTodo").addEventListener("click",changInnerObj2);
-    document.getElementById("navGame").addEventListener("click",changInnerObj3);
-    document.getElementById("navCalculator").addEventListener("click",changInnerObj4);
-    document.getElementById("navJS").addEventListener("click",changInnerObj5);
+    new PageSwitcher();
+});
 
-     let objUebung1 = document.getElementById("obj1");
-     let objUebung2 = document.getElementById("obj2");
-     let objUebung3 = document.getElementById("obj3");
-     let objUebung4 = document.getElementById("obj4");
-     let objUebung5 = document.getElementById("obj5");
+class PageSwitcher {
+    storedLinkAttr = "data";
 
- function changInnerObj1(){
-     objUebung1.classList.add("displBlock");
-     objUebung1.classList.remove("displNone");
-     objUebung2.classList.add("displNone");
-     objUebung2.classList.remove("displBlock");
-     objUebung3.classList.add("displNone");
-     objUebung3.classList.remove("displBlock");
-     objUebung4.classList.add("displNone");
-     objUebung4.classList.remove("displBlock");
-     objUebung5.classList.add("displNone");
-     objUebung5.classList.remove("displBlock");
+    constructor() {
+        this.addNavigationEvents();
+    }
 
- }
+    addNavigationEvents(){
+        // Array.from(document.querySelector(".menu > .navigation.mainNav").children)
+        //     .forEach(item => item.addEventListener("click", this.changeContent.bind(this)));
 
- function changInnerObj2(){
-     objUebung1.classList.add("displNone");
-     objUebung1.classList.remove("displBlock");
-     objUebung2.classList.add("displBlock");
-     objUebung2.classList.remove("displNone");
-     objUebung3.classList.add("displNone");
-     objUebung3.classList.remove("displBlock");
-     objUebung4.classList.add("displNone");
-     objUebung4.classList.remove("displBlock");
-     objUebung5.classList.add("displNone");
-     objUebung5.classList.remove("displBlock");
- }
+        const navContainer = document.querySelector(".menu > .navigation.mainNav");
+        const navItems = navContainer.children;
+        const navItemsAsList = Array.from(navItems);
+        navItemsAsList.forEach(this.addLinkLogic.bind(this));
 
- function changInnerObj3(){
-     objUebung1.classList.add("displNone");
-     objUebung1.classList.remove("displBlock");
-     objUebung2.classList.add("displNone");
-     objUebung2.classList.remove("displBlock");
-     objUebung3.classList.add("displBlock");
-     objUebung3.classList.remove("displNone");
-     objUebung4.classList.add("displNone");
-     objUebung4.classList.remove("displBlock");
-     objUebung5.classList.add("displNone");
-     objUebung5.classList.remove("displBlock");
- }
+        // for(let item of navItems) {
+        //     this.addLinkLogic(item);
+        // }
+    }
 
- function changInnerObj4(){
-     objUebung1.style.display = "none";
-     objUebung2.style.display = "none";
-     objUebung3.style.display = "none";
-     objUebung4.style.display = "block";
-     objUebung5.style.display = "none";
- }
+    addLinkLogic(item) {
+        item.addEventListener("click", this.changeContent.bind(this))
+    }
 
-function changInnerObj5(){
-    objUebung1.style.display = "none";
-    objUebung2.style.display = "none";
-    objUebung3.style.display = "none";
-    objUebung4.style.display = "none";
-    objUebung5.style.display = "block";
+    changeContent(e) {
+        document.getElementById("content")
+            .setAttribute("data", e.target.getAttribute(this.storedLinkAttr));
+    }
 }
 
-});
