00b1f1ea259bd76b3d4216d5d6be9a5f038d7a8a
Justine <justine_kaeslin@sluz.ch>
2022-01-22 09:50:58 +0100

    html: -Seiten grössen angepasst -HTML navi gelöscht js: -Logik für Inhalts wechsel erstellt -navigation über json definiert -per js navigation erstellt

--- js/pageSwitcher.js
+++ js/pageSwitcher.js
@@ -1,6 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     new PageSwitcher();
+    navigationLogic();
 });
+
+const pageContent = '{"pageContent":[ ' +
+   '{"label":"Count Chars", "contentPath":"./content/Uebung1/exercise1.html"},' +
+    '{"label":"ToDo", "contentPath":"./content/Uebung2/todo.html"},' +
+    '{"label":"Rocket", "contentPath":"./content/Uebung3/game.html"},' +
+    '{"label":"Calculator", "contentPath":"./content/Uebung4/calc.html"},' +
+    '{"label":"Snake", "contentPath":"./content/Uebung5/snake.html"},' +
+    '{"label":"Login", "contentPath":"./content/LoginRegister/login.html"},' +
+    '{"label":"Registrieren", "contentPath":"./content/LoginRegister/register.html"}]}';
+
+const obj = JSON.parse(pageContent);
+
+
+function navigationLogic(){
+    for (let i = 0; i < obj.pageContent.length; i++) {
+
+        let pageLabel = obj.pageContent[i].label;
+        let pagePath = obj.pageContent[i].contentPath;
+
+        const li = document.createElement("li");
+        const dFragPage = document.createDocumentFragment();
+        li.textContent = pageLabel;
+        li.setAttribute("data", pagePath );
+        dFragPage.appendChild(li);
+        document.getElementById("navId").appendChild(dFragPage);
+    }
+}
 
 class PageSwitcher {
     storedLinkAttr = "data";
@@ -13,6 +41,7 @@ class PageSwitcher {
         // Array.from(document.querySelector(".menu > .navigation.mainNav").children)
         //     .forEach(item => item.addEventListener("click", this.changeContent.bind(this)));
 
+
         const navContainer = document.querySelector(".menu > .navigation.mainNav");
         const navItems = navContainer.children;
         const navItemsAsList = Array.from(navItems);
@@ -23,8 +52,8 @@ class PageSwitcher {
         // }
     }
 
-    addLinkLogic(item) {
-        item.addEventListener("click", this.changeContent.bind(this))
+    addLinkLogic(navItem) {
+        navItem.addEventListener("click", this.changeContent.bind(this))
     }
 
     changeContent(e) {
